392933994aa121b6d1ba345ee6d66dc4147db522
paulhendricks <paul.hendricks.2013@owu.edu>
2015-08-25 20:00:29 +0000

    improved documentation

--- R/anonymize.R
+++ R/anonymize.R
@@ -2,9 +2,11 @@
 #'
 #' @param .x a vector.
 #' @param .algo the name of the algorithm.
-#' @param ... additional arguments to be based to \code{salt}.
+#' @param .chars set of characters to salt with.
+#' @param .n_chars an integer; number of characters to salt with.
+#' @param ... additional arguments to be based to \code{hash}.
 #' @return An anonymized version of the data object.
 #' @export
-anonymize <- function(.x, .algo = "sha256", ...){
-  hash(salt(.x, ...), .algo = .algo)
+anonymize <- function(.x, .algo = "sha256", .chars = letters, .n_chars = 5L, ...){
+  hash(salt(.x, .chars = .chars, .n_chars = .n_chars), .algo = .algo, ...)
 }
--- R/hash.R
+++ R/hash.R
@@ -1,13 +1,15 @@
 #' Hash a vector.
 #'
+#' See \code{\link[digest]{digest}} for additional documentation.
+#'
 #' @param .x a vector.
 #' @param .algo the name of the algorithm.
-#' @return An anonymized version of the data object.
+#' @param ... additional arguments to be based to \code{\link[digest]{digest}}.
 #' @export
-hash <- function(.x, .algo = "sha256"){
+hash <- function(.x, .algo = "sha256", ...){
   if(!is.atomic((.x))) stop("Vector must be an atomic vector.")
   unq_hashes <- vapply(unique(.x),
-                       function(object) digest::digest(object, algo = .algo),
+                       function(object) digest::digest(object, algo = .algo, ...),
                        character(1))
   return(unname(unq_hashes[.x]))
 }
