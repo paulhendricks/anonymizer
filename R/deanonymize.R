2cd57bbeb12df3661f5552cbb8622d4570acca0b
paulhendricks <paul.hendricks.2013@owu.edu>
2015-08-28 16:07:51 +0000

    update to arguments and tests - still not working

--- R/deanonymize.R
+++ R/deanonymize.R
@@ -3,10 +3,12 @@
 #' @param .x a vector.
 #' @param .algo the name of the algorithm.
 #' @param .seed an integer to seed the random number generator.
+#' @param .chars set of characters to unsalt with.
+#' @param .n_chars an integer; number of characters to unsalt with.
 #' @param ... additional arguments to be based to \code{unhash}.
 #' @return An deanonymized version of the vector.
 
 #' @export
-deanonymize <- function(.x, .algo = "sha256", .seed = 0, ...){
-  return(unsalt(unhash(.x, .algo = .algo, .seed = .seed, ...), .seed = .seed))
+deanonymize <- function(.x, .algo = "sha256", .seed = 0, .chars = letters, .n_chars = 5L, ...){
+  return(unsalt(unhash(.x, .algo = .algo, .seed = .seed, ...), .seed = .seed, .chars = .chars, .n_chars = .n_chars))
 }
--- tests/testthat/test-deanonymize.R
+++ tests/testthat/test-deanonymize.R
@@ -3,11 +3,11 @@ context("deanonymize()")
 
 set.seed(1)
 test_that("Produces the correct output.", {
-  expect_equal(deanonymize(), 1L)
+  expect_equal(deanonymize(), "1")
 })
 
 test_that("Produces the correct output type.", {
-  expect_is(deanonymize(), "integer")
+  expect_is(deanonymize(letters), "character")
 })
 
 test_that("Produces the correct errors.", {
